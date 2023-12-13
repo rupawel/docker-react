f523b3571cf167462c6559f226cbd78c6b790001
pawel.rusakiewicz <rupawel@gmail.com>
2023-12-13 11:35:06 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          lol lom
         </p>
         <a
           className="App-link"
